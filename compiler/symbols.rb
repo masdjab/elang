e19062076a322cf1ae12bc6097bcd3c0172fc5d6
Heryudi Praja <heryudi.praja@redawning.com>
2020-01-21 21:39:52 +0700

    Added class variable

--- compiler/symbols.rb
+++ compiler/symbols.rb
@@ -14,6 +14,7 @@ module Elang
       Function.reset_index
       Variable.reset_index
       InstanceVariable.reset_index
+      ClassVariable.reset_index
       ClassFunction.reset_index
       
       @symbols = []

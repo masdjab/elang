cadeb5737170aea3fa11f3733d16418cb632b944
Heryudi Praja <heryudi.praja@redawning.com>
2020-01-25 00:43:22 +0700

    Removed unused lines

--- compiler/compiler.rb
+++ compiler/compiler.rb
@@ -1,41 +1,10 @@
 # compiler
 # class responsibility:
-# - build EApplication from code string
-
-# elang linker
-
-=begin
-require './utils/converter'
-#require './compiler/identifier'
-#require './compiler/scope'
-require './compiler/token'
-require './application/application'
-require './application/com_format'
-require './application/image_builder'
-#require './compiler/symbol_request'
-#require './compiler/converter'
-#require './compiler/app_section'
-##require './compiler/app_image'
-require './compiler/lib_loader'
-#require './compiler/object_file'
-require './compiler/parser'
-
-
-src_file = ARGV[0]
-parser = Assembly::Parser.new
-parser.load_lib "compiler/stdlib.bin"
-app = parser.parse File.read(src_file)
-relocator = Elang::RelocationInitializer.new.init(app)
-com_format = Elang::ImageBuilder.new.build(app, Elang::ComFormat.new)
-file = File.new("output.com", "wb")
-file.write com_format.raw_image
-file.close
-=end
+# - convert source code to codeset
 
 require './compiler/parser'
 require './compiler/lexer'
 require './compiler/code_generator'
-require './compiler/linker'
 
 module Elang
   class Compiler
@@ -56,11 +25,6 @@ module Elang
       codeset = codegen.generate_code(nodes)
       
       codeset
-      
-      #linker = Elang::Linker.new
-      #binary = linker.link(codeset)
-      
-      #binary
     end
   end
 end

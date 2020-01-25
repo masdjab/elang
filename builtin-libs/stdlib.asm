92057ac317a7e4c785482645ee2ce10df2d5191c
Heryudi Praja <heryudi.praja@redawning.com>
2020-01-25 18:28:13 +0700

    Fixed wrong function name length

--- builtin-libs/stdlib.asm
+++ builtin-libs/stdlib.asm
@@ -2,21 +2,21 @@ dw code
 
 dw 10
 
-dw _int_add, 9
+dw _int_add, 8
   db "_int_add"
-dw _int_subtract, 14
+dw _int_subtract, 13
   db "_int_subtract"
-dw _int_multiply, 14
+dw _int_multiply, 13
   db "_int_multiply"
-dw _int_divide, 12
+dw _int_divide, 11
   db "_int_divide"
-dw _int_add, 9
+dw _int_add, 8
   db "_int_and"
-dw _int_or, 8
-  db "int_or"
-dw _get_obj_var, 13
+dw _int_or, 7
+  db "_int_or"
+dw _get_obj_var, 12
   db "_get_obj_var"
-dw _set_obj_var, 13
+dw _set_obj_var, 12
   db "_set_obj_var"
 dw _puts, 4
   db "puts"

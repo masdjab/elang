1489adf144625ef7d9207636c7caa8e27753cc74
Heryudi Praja <heryudi.praja@redawning.com>
2020-02-01 21:30:12 +0700

    Added new files

---
+++ compiler/assembly_code_builder.rb
@@ -0,0 +1,19 @@
+module Elang
+  module Assembly
+    class CodeBuilder
+      attr_reader :code, :instructions
+      
+      def initialize
+        @code = ""
+        @instructions = []
+      end
+      def <<(instruction)
+        if !instruction.code.empty?
+          @code = @code + Utils::Converter.hex_to_bin(instruction.code)
+        end
+        
+        @instructions << instruction
+      end
+    end
+  end
+end

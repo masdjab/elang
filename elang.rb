5217a3132e743be19d62bbd57119620edb204e77
Heryudi Praja <heryudi.praja@redawning.com>
2020-02-10 00:26:26 +0700

    Moved compile result info after compile process

--- elang.rb
+++ elang.rb
@@ -5,9 +5,9 @@ source_name = File.basename(source_file)
 source_ext  = File.extname(source_file)
 output_file = source_name[0...-source_ext.length] + ".com"
 
+Elang::Main.new.compile(source_file, output_file)
+
 puts "Elang v1.0"
 puts "Source path: #{File.dirname(source_file)}"
 puts "Source file: #{source_name}"
 puts "Output file: #{output_file}"
-
-Elang::Main.new.compile(source_file, output_file)

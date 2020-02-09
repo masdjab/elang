2c4045bffafb1bc1e614d8444805b03fa544644b
Heryudi Praja <heryudi.praja@redawning.com>
2020-02-09 08:03:57 +0700

    Added mem_resize; changed string data structure to support resize

--- libs/stdlib.asm
+++ libs/stdlib.asm
@@ -1,6 +1,6 @@
 dw code
 
-dw 18
+dw 24
 
 dw mem_block_init, 14
   db "mem_block_init"
@@ -12,6 +12,18 @@ dw mem_get_data_offset, 19
   db "mem_get_data_offset"
 dw alloc_object, 12
   db "alloc_object"
+dw _cbw, 4
+  db "_cbw"
+dw _cwb, 4
+  db "_cwb"
+dw _get_byte_at, 12
+  db "_get_byte_at"
+dw _set_byte_at, 12
+  db "_set_byte_at"
+dw _get_word_at, 12
+  db "_get_word_at"
+dw _set_word_at, 12
+  db "_set_word_at"
 dw _int_pack, 9
   db "_int_pack"
 dw _int_unpack, 11

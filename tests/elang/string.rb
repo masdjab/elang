bbe08ca8d57be6b8b3156b37b255a1cf9432ceb6
Heryudi Praja <heryudi.praja@redawning.com>
2020-02-10 00:27:17 +0700

    Added test for append, lcase, ucase (incomplete)

--- tests/elang/string.rb
+++ tests/elang/string.rb
@@ -1,3 +1,26 @@
-a = "Hello world..."
-puts(a)
-puts("Elang now can print text!!!")
+class String
+  def append(str)
+    str_append(first_block, self, str)
+  end
+  def lcase
+    str_lcase(first_block, self)
+  end
+  def ucase
+    str_ucase(first_block, self)
+  end
+end
+
+#a = "Hello world..."
+#puts(a)
+#puts("Elang now can print text!!!")
+
+a = "Hello "
+b = "world..."
+c = a.append(b)
+puts(c)
+
+d = c.ucase
+puts(d)
+
+e = c.lcase
+puts(e)

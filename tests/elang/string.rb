d8b734bc0c218499fb9a72baccbf951e64291ad1
Heryudi Praja <heryudi.praja@redawning.com>
2020-02-10 22:01:07 +0700

    Added string operation example: append, substr, lcase, ucase

--- tests/elang/string.rb
+++ tests/elang/string.rb
@@ -1,26 +1,39 @@
 class String
-  def append(str)
-    str_append(first_block, self, str)
-  end
   def lcase
     str_lcase(first_block, self)
   end
   def ucase
     str_ucase(first_block, self)
   end
+  def append(str)
+    str_append(first_block, self, str)
+  end
+  def substr(pos, len)
+    str_substr(first_block, self, _int_unpack(pos), _int_unpack(len))
+  end
 end
 
-#a = "Hello world..."
-#puts(a)
-#puts("Elang now can print text!!!")
-
-a = "Hello "
-b = "world..."
+# test merge string
+puts("* Test merge string")
+a = "MisTa"
+b = "KeNlY"
 c = a.append(b)
 puts(c)
+puts("")
 
-d = c.ucase
+# test String.substr
+puts("* Test substr")
+d = c.substr(3, 5)
 puts(d)
+puts("")
 
-e = c.lcase
+# test String.ucase
+puts("* Test ucase")
+e = d.ucase
 puts(e)
+puts("")
+
+# test String.lcase
+puts("* Test lcase")
+f = e.lcase
+puts(f)

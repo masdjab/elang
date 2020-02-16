1575c07658acc77c4f6ec98d48e86d5011e344a4
Heryudi Praja <heryudi.praja@redawning.com>
2020-02-16 19:17:48 +0700

    Fixed function call as function parameter

--- tests/function/test_lexer.rb
+++ tests/function/test_lexer.rb
@@ -2,13 +2,14 @@ require './compiler/parser'
 require './compiler/lexer'
 
     source = <<EOS
-def tambah(a, b)
-  a + b
-end
-
-a = tambah(4, 3)
+puts(3, 5)
+puts(_int_pack(2))
+d = "COMPUTING"
+puts(d.substr(3, 5))
+d.set_name("Bowo")
 EOS
 
-tokens = Elang::Parser.new.parse(source)
-ast_nodes = Elang::Lexer.new.to_sexp_array(tokens)
+parser = Elang::Parser.new
+tokens = parser.parse(source)
+ast_nodes = Elang::Lexer.new.to_sexp_array(tokens, parser.code_lines)
 puts Elang::Lexer.sexp_display(ast_nodes)

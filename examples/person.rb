b9e62c7a4b5efe066b1919402d91f5bbae46e9f0
Heryudi Praja <heryudi.praja@redawning.com>
2020-02-24 21:45:30 +0700

    Removed unused lines

--- examples/person.rb
+++ examples/person.rb
@@ -19,12 +19,10 @@ end
 
 p1 = Person.new
 p1.set_age(32)
-a = p1.get_age()
 puts("p1.age = ".concat(p1.get_age().to_s()))
 
 p2 = Programmer.new
 p2.set_age(21)
 p2.set_lang("Ruby")
-b = p2.get_lang
 puts("p2.age = ".concat(p2.get_age().to_s()))
 puts("p2.lang = ".concat(p2.get_lang()))

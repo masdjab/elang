5b7059e5626ce69cd7fb1af66dc2e0514190b855
Heryudi Praja <heryudi.praja@redawning.com>
2020-05-04 00:02:24 +0700

    Added test lib

---
+++ libs/stdlibxx.asm
@@ -0,0 +1,12 @@
+include 'fasm16.asm'
+
+INT_MIN_MASK1             EQU 8000h
+INT_MIN_MASK2             EQU 4000h
+
+macro var [varargs] {dw varargs}
+
+var table
+var 7 dup 0
+
+include 'tstproc.asm'
+include 'stdtable.asm'

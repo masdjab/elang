c08dc32887f16607d4d28c9877aa6c2100c1f8d2
Heryudi Praja <heryudi.praja@redawning.com>
2020-05-04 00:03:14 +0700

    Added options -no-elang-lib and -stdlib

--- compiler/command_interface.rb
+++ compiler/command_interface.rb
@@ -28,13 +28,11 @@ module Elang
       puts "Usage: ruby elang.rb source_file [options]"
       puts
       puts "Available options:"
-      puts "-d    Enable dev mode"
-      puts "-nn   Nodes output to show: none"
-      puts "-nl   Nodes output to show: libs"
-      puts "-nu   Nodes output to show: user"
-      puts "-na   Nodes output to show: all"
-      puts "-h    Show this help"
-      puts "/?    Alias for -h"
+      puts "-d              Enable dev mode"
+      puts "-n=mode         Nodes output to show: none, libs, user, all"
+      puts "-stdlib=file    Specify stdlib file"
+      puts "-no-elang-lib   Do not include lib.elang"
+      puts "-h or /?        Show this help"
     end
     def self.display_error(msg)
       puts msg
@@ -44,31 +42,67 @@ module Elang
       nl = args.delete("-nl")
       nu = args.delete("-nu")
       na = args.delete("-na")
+      tl = args.delete("-tl")
+      
       mm = {"-nn" => :none, "-nl" => :libs, "-nu" => :user, "-na" => :all}
+      
       [nn, nl, nu, na].select{|x|!x.nil?}.inject({}){|a,b|a[b] = mm[b]; a}
     end
+    def self.parse_arguments(arguments)
+      args = []
+      opts = {}
+      
+      arguments.each do |a|
+        if "/-".index(a[0])
+          a = a.index("--") == 0 ? a[2..-1] : a[1..-1]
+          m = a.index(":")
+          n = a.index("=")
+          
+          if (s = m ? m : n)
+            k = a[0...s].strip
+            v = a[(s + 1)..-1].strip
+            v = v[1..-2] if (v[0] == v[1]) && ["\"", "'"].index(v[0])
+            opts[k] = v
+          else
+            opts[a] = nil
+          end
+        else
+          a = a[1..-2] if (a[0] == a[-1]) && ["\"", "'"].index(a[0])
+          args << a
+        end
+      end
+      
+      [args, opts]
+    end
     def self.compile
-      args = ARGV
+      args, opts = self.parse_arguments(ARGV)
+      valid_options = ["d", "n", "stdlib", "no-elang-lib", "h", "?"]
       
       self.display_title
       
-      if (args = ARGV).count == 0
+      if args.empty?
+        self.display_usage
+      elsif !(invalid_options = opts.keys - valid_options).empty?
+        self.display_error "Invalid options: #{invalid_options.map{|x|"-#{x}"}.join(", ")}.\r\n#{HELP_HINT}"
+      elsif opts.key?("h") || opts.key?("?")
         self.display_usage
       else
         source_file = args.shift
-        show_help = args.delete("-h"){args.delete("/?"){false}}
-        dev_mode = args.delete("-d"){false}
-        show_nodes = get_show_nodes_params(args)
+        show_nodes = opts["n"]
+        stdlib = opts["stdlib"]
+        dev_mode = opts.key?("d")
+        no_elang_lib = opts.key?("no-elang-lib")
         
-        if !args.empty?
-          self.display_error "Invalid options: #{args.join(", ")}.\r\n#{HELP_HINT}"
-        elsif show_help
-          self.display_usage
-        elsif show_nodes.count > 1
-          self.display_error "Invalid show_nodes options: #{show_nodes.keys.join(" ")}.\r\n#{HELP_HINT}"
+        if !show_nodes.nil? && !["none", "libs", "user", "all"].include?(show_nodes)
+          self.display_error "Invalid show_nodes options: #{show_nodes}.\r\n#{HELP_HINT}"
         else
-          options = {dev: dev_mode}
-          options[:show_nodes] = show_nodes.values.first if !show_nodes.empty?
+          options = {}
+          
+          options[:dev] = true if dev_mode
+          options[:show_nodes] = show_nodes if show_nodes
+          options[:stdlib] = stdlib if stdlib
+          options[:no_elang_lib] = no_elang_lib if no_elang_lib
+          
           self.new.compile(source_file, options)
         end
       end

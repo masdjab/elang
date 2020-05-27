17d5fc8df432642a997bd6d8464888b8417c34c8
Heryudi Praja <heryudi.praja@redawning.com>
2020-05-27 17:47:56 +0700

    Fixed method dispatcher

--- compiler/method_dispatcher/method_dispatcher_32.rb
+++ compiler/method_dispatcher/method_dispatcher_32.rb
@@ -38,7 +38,7 @@ module Elang
         cls[:i_funs].each do |f|
           function = codepad.symbols.items.find{|x|(x.is_a?(Function)) && (x.scope.cls == key) && (x.name == f[:name]) && x.receiver.nil?}
           codepad.add_function_id_ref f[:name], codepad.code_len + 1
-          codepad.add_abs_code_ref function, codepad.code_len + 6
+          codepad.add_abs_code_ref function, codepad.code_len + 8
           codepad.append_hex "3D00000000"         # cmp ax, #{f[:id]}
           codepad.append_hex "7506"               # jnz + 2
           codepad.append_hex "B800000000"         # mov ax, #{key.downcase}_obj_#{f[:name]}
@@ -98,15 +98,15 @@ module Elang
       codepad.append_hex "50"                     # push ax
       codepad.append_hex "56"                     # push si
       codepad.append_hex "89EE"                   # mov si, bp
-      codepad.append_hex "8B4510"                 # mov ax, [bp + 8]
+      codepad.append_hex "8B4510"                 # mov ax, [bp + 16]
       codepad.append_hex "83C004"                 # add ax, 4
-      codepad.append_hex "D1E0"                   # shl ax, 1
+      codepad.append_hex "C1E002"                 # shl ax, 2
       codepad.append_hex "01C6"                   # add si, ax
-      codepad.append_hex "8B4504"                 # mov ax, [bp + 2]
+      codepad.append_hex "8B4504"                 # mov ax, [bp + 4]
       codepad.append_hex "87EE"                   # xchg bp, si
       codepad.append_hex "894500"                 # mov [bp], ax
       codepad.append_hex "87EE"                   # xchg bp, si
-      codepad.append_hex "897504"                 # mov [bp + 2], si
+      codepad.append_hex "897504"                 # mov [bp + 4], si
       codepad.append_hex "5E"                     # pop si
       codepad.append_hex "58"                     # pop ax
       codepad.append_hex "5D"                     # pop bp

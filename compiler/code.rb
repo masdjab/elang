6d37c41559e5efc67ef9e57d78b796338dbbf33a
Heryudi Praja <heryudi.praja@redawning.com>
2020-06-08 00:17:22 +0700

    Fixed incorrect string padding in Code.align

--- compiler/code.rb
+++ compiler/code.rb
@@ -1,8 +1,10 @@
 module Elang
   class Code
-    def self.align(code, align_size = 16)
-      if (extra_size = (code.length % 16)) > 0
-        code = code + (0.chr * (16 - extra_size))
+    def self.align(code, align_size = 16, pad_char = nil)
+      pc = pad_char ? pad_char[0, 1] : 0.chr
+      
+      if (extra_size = (code.length % align_size)) > 0
+        code = code + (pc * (align_size - extra_size))
       end
       
       code

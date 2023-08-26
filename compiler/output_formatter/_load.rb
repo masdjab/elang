d0acced0e2254041bf59de9906808ae03a4d56cf
masdjab <masdjab@gmail.com>
2023-08-26 18:14:56 +0700

    Removed PE Header

--- compiler/output_formatter/_load.rb
+++ compiler/output_formatter/_load.rb
@@ -1,6 +1,5 @@
 require_relative 'base_formatter'
 require_relative 'mz_header'
-require_relative 'pe_header'
 require_relative 'com_formatter'
 require_relative 'mz_formatter'
 require_relative 'dx_formatter'
